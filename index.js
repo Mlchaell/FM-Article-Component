a687b32ddaad38e65acc0bd04ccd91f2c2c74698
Michael <44348788+Mlchaell@users.noreply.github.com>
2024-04-23 16:59:14 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// let openShareModalBtn = document.querySelector("[data-openModal]");
+// let closeShareModalBtn = document.querySelector("[data-closeModal]");
+// let shareModal = document.querySelector(".share-modal");
+
+// openShareModalBtn.addEventListener("click", () => {
+  
+// })
