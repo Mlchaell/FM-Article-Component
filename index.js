1615bb93549ff97f1e50fff8ec0a4af684ff1c46
Michael <44348788+Mlchaell@users.noreply.github.com>
2024-04-26 14:17:01 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-// let openShareModalBtn = document.querySelector("[data-openModal]");
-// let closeShareModalBtn = document.querySelector("[data-closeModal]");
-// let shareModal = document.querySelector(".share-modal");
+let shareBtn = document.querySelector(".article__share-btn");
 
-// openShareModalBtn.addEventListener("click", () => {
-  
-// })
+let shareModal = document.querySelector(".share-modal");
+let authorInfo = document.querySelector(".content__author");
+
+function getScreenType() {
+  let screenWidth = screen.width;
+
+  if (screenWidth >= 950) {
+    return "desktop"
+  } else {
+    return "mobile"
+  }
+}
+
+shareBtn.addEventListener("click", e => {
+  if (shareBtn.getAttribute("aria-expanded") === "false") {
+    // Displays share menu
+    shareBtn.setAttribute("aria-expanded", "true");
+
+    if (getScreenType() === "mobile") {
+      shareModal.classList.toggle("hidden");
+      authorInfo.classList.toggle("hidden");  
+    } else if (getScreenType() === "desktop") {
+      shareModal.classList.toggle("hidden");
+    }
+  } else {
+    // Hides share menu
+    shareBtn.setAttribute("aria-expanded", "true");
+
+    if (getScreenType() === "mobile") {
+      shareModal.classList.toggle("hidden");
+      authorInfo.classList.toggle("hidden");  
+    } else if (getScreenType() === "desktop") {
+      shareModal.classList.toggle("hidden");
+    }
+  }
+})
